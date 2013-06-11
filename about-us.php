a02c0890963798fce00afa9a7d8bd9406cec49ec
Allison Koberstein <allison@allisonkoberstein.com>
2013-06-11 16:06:21 -0700

    About Us styled
    Still need to write the content on left side. Maybe should have
    image(s)?

--- about-us.php
+++ about-us.php
@@ -17,7 +17,7 @@
 	    	<h2>
 	    		Serving the local community
 	    	</h2>
-	    	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	    	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 	    	</p>
     	</section>
     	<section id="part2">
@@ -26,7 +26,7 @@
     		</h2>
 	 		<div class="founder">
 	 			<img src="images/founder_1.jpg" />
-	 			<h3>Jamie Wei <span>Senior Pilot</span></h3>
+	 			<h3>Jamie Wei <span class="job">Senior Pilot</span></h3>
 	 			<p>
 	 				Rumor has it that Jamie was bitten by a radioactive pigeon when he was 5 years old, giving him an uncanny aeronautical ability and an insatiable desire for bread crumbs. He's been a licensed pilot for 15 years and attends the occasional local comic convention.
 	 			</p>
@@ -36,7 +36,7 @@
 	 		</div>
 	 		<div class="founder">
 	 			<img src="images/founder_2.jpg" />
-	 			<h3>Soheil Yavari <span>Operations and Marketing</span></h3>
+	 			<h3>Soheil Yavari <span class="job">Operations</span></h3>
 	 			<p>
 	 				After 6 years managing YVR airport, I decided government work wasn't for me. When Jamie proposed the idea for us to go into business together, I left it all behind. Sky Air is my labor of love.  
 	 			</p>
