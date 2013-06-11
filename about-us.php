3b44c9f6415ebd9ac088880907df1389cf7b1be7
Allison Koberstein <allison@allisonkoberstein.com>
2013-06-11 00:50:50 -0700

    About Us and Contact Us base html (no style)
    Created HTML and most of the writing for these two pages. Added photos
    of the founders (edited from CC photos I found). The pages look like a
    mess since I have not styled them at all yet.

--- about-us.php
+++ about-us.php
@@ -5,6 +5,46 @@
 	<?php include 'nav.php' ?>
     
     <div class="main" id="about-us">
+    	<section id="part1">
+	    	<h1>
+	    		About Us
+	    	</h1>
+	    	<h2>
+	    		Flying is in our blood
+	    	</h2>
+	    	<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+	    	</p>
+	    	<h2>
+	    		Serving the local community
+	    	</h2>
+	    	<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	    	</p>
+    	</section>
+    	<section id="part2">
+    		<h2>
+    			The Founders
+    		</h2>
+	 		<div class="founder">
+	 			<img src="images/founder_1.jpg" />
+	 			<h3>Jamie Wei <span>Senior Pilot</span></h3>
+	 			<p>
+	 				Rumor has it that Jamie was bitten by a radioactive pigeon when he was 5 years old, giving him an uncanny aeronautical ability and an insatiable desire for bread crumbs. He's been a licensed pilot for 15 years and attends the occasional local comic convention.
+	 			</p>
+	 			<p>
+	 				Email: <a href="mailto:jamie@skyair.com">jamie@skyair.com</a>
+	 			</p>
+	 		</div>
+	 		<div class="founder">
+	 			<img src="images/founder_2.jpg" />
+	 			<h3>Soheil Yavari <span>Operations and Marketing</span></h3>
+	 			<p>
+	 				After 6 years managing YVR airport, I decided government work wasn't for me. When Jamie proposed the idea for us to go into business together, I left it all behind. Sky Air is my labor of love.  
+	 			</p>
+	 			<p>
+	 				Email: <a href="mailto:soheil@skyair.com">soheil@skyair.com</a>
+	 			</p>
+	 		</div>
+    	</section>
     </div>
     
     <?php include 'footer.php' ?>
--- contact-us.php
+++ contact-us.php
@@ -5,6 +5,44 @@
 	<?php include 'nav.php' ?>
     
     <div class="main" id="contact-us">
+    	<section id="part1">
+	    	<h1>
+	    		Contact Us
+	    	</h1>
+	    	<p>Feel free to contact us at any time. We'd love to hear from you!
+	    	</p>
+	    	<ul>
+	    		<li>Email: <a href="mailto:hello@skyair.com">hello@skyair.com</a></li>
+	    		<li>Phone: 604.888.8888</li>
+	    	</ul>
+	    	<hr />
+	    	<form>
+	    		<label><h2>Name</h2></label>
+                <input type="text" id="name">
+
+	    		<label><h2>Email</h2></label>
+                <p class="info note">*We need this so we can respond to you.</a>
+                <input type="email" id="email" required>
+
+	    		<label><h2>Your Message</h2></label>
+                <span class="note">Please note any special requests.</span>
+                <textarea rows="4" cols="22" id="message" required></textarea>
+                <a href="#" class="button highlighted">SUBMIT</a>
+	    	</form>
+	    	
+    	</section>
+    	<section id="part2">
+			<h2>
+	    		Where We Are
+	    	</h2>
+    		<p>1111 Mountain Pkwy<br />
+				Burnaby BC<br />
+				A1I 2R3
+	    	</p>
+	    	<p>
+	    		[MAP GOES HERE]
+	    	</p>
+    	</section>
     </div>
     
     <?php include 'footer.php' ?>
