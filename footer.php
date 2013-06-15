6a46148395dbe3d3c01e32fd47b7e794eebe3844
Scott Inglis <cyborg771@gmail.com>
2013-06-15 16:51:02 -0700

    First round of tablet designs
    Home and About Us working properly. Style.css split into 4 stylesheets.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 	<footer>
     	<div class="footer-info">
-        	<h2><a href="faq.php">Information:</a></h2>
+        	<h2 id="f-information"><a href="faq.php">Information:</a></h2>
             <ul>
             	<li><a href="tours-packages.php">Prices</a></li>
                 <li><a href="flight-schedule.php">Schedule</a></li>
@@ -10,7 +10,7 @@
             </ul>
         </div>
         <div class="footer-info">
-        	<h2><a href="faq.php">Policies:</a></h2>
+        	<h2 id="f-policies"><a href="faq.php">Policies:</a></h2>
             <ul>
             	<li><a class="question" id="safety_footer" href="faq.php#safety">Safety</a></li>
                 <li><a class="question" id="cancellation_footer" href="faq.php#cancellation">Cancellation</a></li>
@@ -18,14 +18,14 @@
             </ul>
         </div>
         <div class="footer-info">
-        	<h2><a href="contact-us.php">Contact:</a></h2>
+        	<h2 id="f-contact"><a href="contact-us.php">Contact:</a></h2>
             <ul>
             	<li>604.888.8888</li>
                 <li><a href="mailto:hello@skyair.com">hello@skyair.com</a></li>
             </ul>
         </div>
         <div class="footer-info">
-        	<h2>Address:</h2>
+        	<h2 id="f-address">Address:</h2>
             <ul>
             	<li><a href="https://maps.google.ca/maps?q=1111+mountain+pkwy+burnaby&hl=en&sll=49.274191,-122.936059&sspn=0.011928,0.033023&t=h&hnear=1111+Burnaby+Mountain+Pkwy,+Burnaby,+Greater+Vancouver,+British+Columbia&z=16">1111 Mountain Pkwy<br />Burnaby BC<br />A1I 2R3</a></li>
             </ul>
--- head.php
+++ head.php
@@ -5,6 +5,7 @@
     <title>Sky Air - Vancouver Helicopter Tours</title>
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600" rel="stylesheet" type="text/css">
     <link href="normalize.css" rel="stylesheet" type="text/css" />
-    <link href="style.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/shared.css" rel="stylesheet" type="text/css" />
+    <link href="CSS/tablet.css" rel="stylesheet" type="text/css" />
     <script src="http://code.jquery.com/jquery-latest.js"></script>
 </head>
