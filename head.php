91d84ad2b818114abdbd69e6f721e8bb47a840e2
Allison Koberstein <allison@allisonkoberstein.com>
2013-06-18 01:32:16 -0700

    Dynamic scaling, mobile base layout, css
    Consolidated some of the css that was the same in both desktop.css and
    tablet.css into shared.css

--- head.php
+++ head.php
@@ -6,6 +6,9 @@
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600" rel="stylesheet" type="text/css">
     <link href="normalize.css" rel="stylesheet" type="text/css" />
     <link href="CSS/shared.css" rel="stylesheet" type="text/css" />
-    <link href="CSS/tablet.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" media="screen and (min-width: 900px)" href="CSS/desktop.css"> 
+	<link rel="stylesheet" media="screen and (min-width: 501px) and (max-width: 899px)" href="CSS/tablet.css">
+	<link rel="stylesheet" media="screen and (max-width: 500px)" href="CSS/mobile.css">
+    <!-- <link href="CSS/tablet.css" rel="stylesheet" type="text/css" /> -->
     <script src="http://code.jquery.com/jquery-latest.js"></script>
 </head>
--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
     <header>
         <a href="index.php" id="logo">
             <img src="images/logo.png" alt="Sky Air"/>
-        <a>
+        </a>
         <nav>
             <ul>
                 <li><a href="book-now-1.php">Book Now</a></li>
@@ -11,5 +11,16 @@
                 <li><a href="faq.php">FAQ</a></li>
                 <li><a href="contact-us.php">Contact Us</a></li>
             </ul>
+            <form>
+                <select name="menu" onChange="top.location.href = this.form.menu.options[this.form.menu.selectedIndex].value;
+return false;">  <!-- This line of javascript makes the form submit automatically on selection, without having to press a button. Learned from here: http://www.mediacollege.com/internet/javascript/navigation/drop-menu.html -->
+                    <option value="#" selected>Navigation</option>
+                    <option value="book-now-1.php">Book Now</option>
+                    <option value="flight-schedule.php">Flight Schedule</option>
+                    <option value="about-us.php">About Us</option>
+                    <option value="faq.php">FAQ</option>
+                    <option value="contact-us.php">Contact Us</option>
+                </select>
+            </form>
         </nav>
     </header>
