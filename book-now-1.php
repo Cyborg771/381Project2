41dcce20075cf146c74f386b976db224d1a6ab1f
Scott Inglis <cyborg771@gmail.com>
2013-06-26 13:24:13 -0700

    Date Pickers Fixed

--- book-now-1.php
+++ book-now-1.php
@@ -66,7 +66,58 @@
                 <div class="form-column" id="column1">
                     <label><h2>Date</h2></label>
                     <a class="info note" href="flight-schedule.php">Check Flight Schedule &raquo;</a>
-                    <input type="date" id="FlightDate" required>
+                    <select class="editable date" id="month">
+                                <option value="JAN">Jan</option>
+                                <option value="JAN">Feb</option>
+                                <option value="JAN">Mar</option>
+                                <option value="JAN">Apr</option>
+                                <option value="JAN">May</option>
+                                <option value="JAN">Jun</option>
+                                <option value="JAN">Jul</option>
+                                <option value="JAN">Aug</option>
+                                <option value="JAN">Sep</option>
+                                <option value="JAN">Oct</option>
+                                <option value="JAN">Nov</option>
+                                <option value="JAN">Dec</option>
+                            </select>
+                            <select class="editable date" id="day">
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                                <option value="4">4</option>
+                                <option value="5">5</option>
+                                <option value="6">6</option>
+                                <option value="7">7</option>
+                                <option value="8">8</option>
+                                <option value="9">9</option>
+                                <option value="10">10</option>
+                                <option value="11">11</option>
+                                <option value="12">12</option>
+                                <option value="13">13</option>
+                                <option value="14">14</option>
+                                <option value="15">15</option>
+                                <option value="16">16</option>
+                                <option value="17">17</option>
+                                <option value="18">18</option>
+                                <option value="19">19</option>
+                                <option value="20">20</option>
+                                <option value="21">21</option>
+                                <option value="22">22</option>
+                                <option value="23">23</option>
+                                <option value="24">24</option>
+                                <option value="25">25</option>
+                                <option value="26">26</option>
+                                <option value="27">27</option>
+                                <option value="28">28</option>
+                                <option value="29">29</option>
+                                <option value="30">30</option>
+                                <option value="31">31</option>
+                            </select>
+                            <select class="editable date" id="year">
+                                <option value="1">2015</option>
+                                <option value="2">2014</option>
+                                <option value="3">2013</option>
+                            </select>
                     <label><h2>Flight Time</h2></label>
                     <a class="info note" href="flight-schedule.php">Check Flight Schedule &raquo;</a>
                     <select id="FlightTime" required>
